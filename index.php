9393db765de4c0f8622018a1a1a9285274d54fc9
Gaotian <gaotian@b5m.com>
2013-09-25 17:53:00 +0800

    add index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    define ('APP_NAME','JunliSocket');
+    define('APP_DEBUG',true); // 开启调试模式
+    //定义项目路径
+    /*define('APP_PATH', '');*/
+    define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
+    require './tphp/ThinkPHP.php';
+?>
