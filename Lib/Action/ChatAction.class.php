2536106bf549b8b24b03735f876723b9f0784107
Gaotian <gaotian@b5m.com>
2013-09-26 14:23:02 +0800

    add chat

---
+++ Lib/Action/ChatAction.class.php
@@ -0,0 +1,10 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class ChatAction extends Action {
+    function indexAct(){
+        $this->display("Tpl/index.html");
+    }
+    function inAct(){
+        $this->display("Tpl/chatin.html");
+    }
+}
